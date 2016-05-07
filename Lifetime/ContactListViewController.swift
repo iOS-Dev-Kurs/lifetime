79f1af9d94d2ea4fc35186e464a003c685c1d891
ChrisBtt <chris-blattgerste@gmx.de>
2016-05-08 00:25:10 +0200

    Funktioniert mit Erweiterung

--- Lifetime/ContactListViewController.swift
+++ Lifetime/ContactListViewController.swift
@@ -69,20 +69,62 @@ class ContactListViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         switch segue.identifier! {
 
-        // TODO: prepare segue.destinationViewController for each identifier
+        case "showContactDetail":
+            guard let indexPath = self.tableView.indexPathForSelectedRow else {break}
+            let contact = contacts[indexPath.row]
+            let contactDetailViewController = segue.destinationViewController as! ContactDetailViewController
+            contactDetailViewController.contact = contact
             
         default:
             break
         }
     }
     
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        switch identifier {
+        case "showContactDetail":
+            guard let indexPath = self.tableView.indexPathForSelectedRow else {break}
+            let contact = contacts[indexPath.row]
+            
+            if contact.lifetime != nil {
+                return true
+            } else {
+                return false
+            }
+        default:
+            break
+        }
+        return true
+    }
 }
 
 
 // MARK: - Table View Datasource
 
-// TODO: implement UITableViewDatasource protocol
-
+extension ContactListViewController {
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("LifetimeCell", forIndexPath: indexPath) as! LifetimeCell
+        let contact = contacts[indexPath.row]
+        cell.configureForContact(contact)
+        if contact.lifetime != nil {
+            cell.selectionStyle = .Default
+            cell.accessoryType = .DisclosureIndicator
+        } else {
+            cell.selectionStyle = .None
+            cell.accessoryType = .None
+        }
+        return cell
+    }
+}
 
 // MARK: - Search Results Updating
 
---
+++ Lifetime/LifetimeCell.swift
@@ -0,0 +1,28 @@
+//
+//  LifetimeCell.swift
+//  Lifetime
+//
+//  Created by Christoph Blattgerste on 06.05.16.
+//  Copyright © 2016 iOS Dev Kurs Universität Heidelberg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Contacts
+
+class LifetimeCell : UITableViewCell {
+    
+    func configureForContact(contact: CNContact) {
+        
+        textLabel?.text = CNContactFormatter.stringFromContact(contact, style: .FullName)
+        if let lifetime = contact.lifetime {
+            let lifetimeFormatter = NSDateComponentsFormatter()
+            lifetimeFormatter.allowedUnits = .Day
+            lifetimeFormatter.unitsStyle = .Full
+            detailTextLabel?.text = lifetimeFormatter.stringFromTimeInterval(lifetime)
+        } else {
+            detailTextLabel?.text = nil
+        }
+    }
+}
+
